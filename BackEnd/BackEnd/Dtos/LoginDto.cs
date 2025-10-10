de75b7423c96a2c8a373e83cacf80104825deb1e
SharkDrown <ht271153@gmail.com>
2025-10-10 22:54:15 +0700

    Xóa thư mục không cần thiết khỏi project

--- BackEnd/BackEnd/Dtos/LoginDto.cs
+++
@@ -1,9 +0,0 @@
-namespace BackEnd.Models
-{
-    public class LoginDto
-    {
-        public string TenDangNhap { get; set; } = string.Empty;
-        public string MatKhau { get; set; } = string.Empty;
-        
-    }
-}
