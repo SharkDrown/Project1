d6d36ccded9434ea541e3014cc7aaab0c9e41d01
SharkDrown <ht271153@gmail.com>
2025-10-21 10:56:08 +0700

    fix frontend

--- code/BackEnd/Models/QuanLyThuVienContext.cs
+++ code/BackEnd/Models/QuanLyThuVienContext.cs
@@ -51,7 +51,7 @@ public partial class QuanLyThuVienContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=LAPTOP-HUNGVIET\\SQLEXPRESS;Database=QuanLyThuVien;Trusted_Connection=True;TrustServerCertificate=True;");
+        => optionsBuilder.UseSqlServer("Data Source=HOANG;Initial Catalog=QuanLyThuVien;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
